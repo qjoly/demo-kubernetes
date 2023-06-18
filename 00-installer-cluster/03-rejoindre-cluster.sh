3c89634623a2fa54f814e848ea5634d90e7e8e0b
QJoly <github@thoughtless.eu>
2023-06-18 07:09:26 +0200

    Ajout: Ligne vide (lint bash)

--- 00-installer-cluster/03-rejoindre-cluster.sh
+++ 00-installer-cluster/03-rejoindre-cluster.sh
@@ -2,4 +2,4 @@
 hostnamectl set-hostname client01
 reboot
 # K3S_URL -> https://192.168.1.102:6443 dans mon cas (IP de Master01)
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.1.102:6443 K3S_TOKEN=K105c569b652e41bcda103d1bf134bdaa93ead4a8bb6f70f14a49fbb87557ff0ea3::server:dd364aeffbb60704017e3695d5385ad4 sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.1.102:6443 K3S_TOKEN=K10008a5661a10aaf6563768c74895fb9c239cbb7b6a1d5939d8652698c591e62bb::server:16e2014f9356b7667ca61ee85fbe6ce4 sh -
